fce4ef08c46acfe5e99dc49f1bb57cc6c131d02c
Troy Lopez <troy.n.lopez@gmail.com>
2024-02-01 01:28:31 -0800

    need to handle primes and clean up

--- hw2.asm
+++ hw2.asm
@@ -1,142 +1,193 @@
 TITLE Assignment2 (hw2.asm)
 
 ; Troy Lopez
-; CS 271 Assignment1		1/18/24
+; CS 271 Assignment1     1/18/24
 ; Gets user input and calculates factors
 
 INCLUDE Irvine32.inc
 
 ; constants go here
-COLON	equ	": ",0
-
+;COLON   equ ": ",0
+LIM     equ 1001    ; 1001 so check can be inclusive
 
 .data
-	;variable definitions
-	; Notation:
-	; prThing is a string to be printed
-	; usThing is a user inputted value
-	; with the exception of intro, all values not prefixed by pr or us are the results of calcs
+    ; variable definitions
+    ; Notation:
+    ; prThing is a string to be printed
+    ; usThing is a user inputted value
+    ; with the exception of intro, all values not prefixed by pr or us are the results of calcs
 
+    intro1  BYTE "Hw2: Factors, by Troy Lopez", 0
+    intro2  BYTE "This program calculates and displays the factors of numbers between upper and lower bounds, as well as when numbers are prime.", 0
+    
+    prName  BYTE "Enter your name: ", 0
+    prLowr  BYTE "Enter a number between 1 and 1000 for the lower range: ", 0
+    prHigh  BYTE "Enter a number between 1 and 1000 for the higher range: ", 0
+    prAgin  BYTE "Would you like to do another calculation? (1 yes/0 no)", 0
+    prLoLo  BYTE "Sorry, that value is too low, please try again", 0
+    prHiLo  BYTE "Sorry, that value is too high, please try again", 0
+    prLoHi  BYTE "Input must be >= lower bound, please try again", 0
 
-	intro1	BYTE	"Hw2: Factors, by Troy Lopez", 0
-	intro2	BYTE	"This program calculates and displays the factors of numbers between upper and lower bounds, as well as when numbers are prime.", 0
-	prName	BYTE	"Enter your name: ", 0
-	prLowr	BYTE	"Enter a number betwen 1 and 1000 for the lower range: ", 0
-	prHigh	BYTE	"Enter a number between 1 and 1000 for the higher range: ", 0
-	prAgin	BYTE	"Would you like to do another calculation? (1 yes/0 no)", 0
-	prLoLo	BYTE	"Sorry, that value is too low, please try again", 0
-	prHiLo	BYTE	"Sorry, that value is too high, please try again", 0
-	prLoHi	BYTE	"Input must be >= lower bound, please try again", 0
+    usName  BYTE 33 DUP(0)
+    usLowr  DWORD ?
+    usHigh  DWORD ?
 
-	usName	BYTE	33	DUP(0)
-	usLowr	DWORD	?
-	usHigh	DWORD	?
+    loopCt  DWORD ?
+    colon   BYTE ": ",0
+    space   BYTE " ",0
 
 .code
-	main PROC
+main PROC
 
-	;intro program
-	mov		edx, OFFSET intro1
-	call	WriteString
-	call	Crlf
-	mov		edx, OFFSET intro2
-	call	WriteString
-	call	Crlf
+    ; intro program
+    mov     edx, OFFSET intro1
+    call    WriteString
+    call    Crlf
+    mov     edx, OFFSET intro2
+    call    WriteString
+    call    Crlf
 
-	;get name
-	mov		edx, OFFSET prName
-	call	WriteString
-	
-	mov		edx, OFFSET usName
-	mov		ecx, 32
-	call	ReadString
-	call	Crlf
+    ; get name
+    mov     edx, OFFSET prName
+    call    WriteString
+    
+    mov     edx, OFFSET usName
+    mov     ecx, 32
+    call    ReadString
+    call    Crlf
 
 
-	; get lower bound
+    ; get lower bound
 getLower:
-	mov		edx, OFFSET prLowr
-	call	WriteString
-	
-	call	ReadInt				;stores in eax
-	mov		usLowr, eax
-	call	crlf
-	
-	; check if too low
-	cmp		usLowr, 0
-	jle		lowerTooLow			; if 0 or below
-	
-	cmp		usLowr, 1001
-	jge		lowerTooHigh
-	jmp getHigher
+    mov     edx, OFFSET prLowr
+    call    WriteString
+    
+    call    ReadInt             ; stores in eax
+    mov     usLowr, eax
+    call    Crlf
+    
+    ; check if too low
+    cmp     usLowr, 1
+    jl      lowerTooLow         ; if below 1
+    
+    cmp     usLowr, 1000
+    jg      lowerTooHigh
+    jmp     getHigher
 
 
-	; if user input for lower bound is too low
+    ; if user input for lower bound is too low
 lowerTooLow:
-	mov		edx, OFFSET	prLoLo
-	call	WriteString
-	call	Crlf
-	jmp		getLower
+    mov     edx, OFFSET prLoLo
+    call    WriteString
+    call    Crlf
+    jmp     getLower
 
 
-	; if too high
+    ; if too high
 lowerTooHigh:
-	mov		edx, OFFSET prHiLo
-	call	WriteString
-	call	Crlf
-	jmp		getLower
+    mov     edx, OFFSET prHiLo
+    call    WriteString
+    call    Crlf
+    jmp     getLower
 
-	; get higher bound
+    ; get higher bound
 getHigher:
-	mov		edx, OFFSET prHigh
-	call	WriteString
-	
-	call	ReadInt				;stores in eax
-	mov		usHigh, eax
-	call	crlf
-	
-	; check if too low
-	mov		eax, usLowr
-	cmp		usHigh, eax
-	jle		higherToolow		; if 0 or below
-	
-	cmp		usHigh, 1001
-	jge		higherTooHigh
-	jmp doMath
+    mov     edx, OFFSET prHigh
+    call    WriteString
+    
+    call    ReadInt             ; stores in eax
+    mov     usHigh, eax
+    call    Crlf
+    
+    ; check if too low
+    mov     eax, usLowr
+    cmp     usHigh, eax
+    jl      higherTooLow       ; if below lower bound
+    
+    cmp     usHigh, LIM
+    jg      higherTooHigh
+    jmp     prepMath
 
 
-	; if user input for lower bound is too low
+    ; if user input for lower bound is too low
 higherTooLow:
-	mov		edx, OFFSET	prLoHi
-	call	WriteString
-	call	Crlf
-	jmp		getHigher
+    mov     edx, OFFSET prLoHi
+    call    WriteString
+    call    Crlf
+    jmp     getHigher
 
 
-	; if too high
+    ; if too high
 higherTooHigh:
-	mov		edx, OFFSET prHiLo	;reuse message from too low
-	call	WriteString
-	call	Crlf
-	jmp		getHigher
+    mov     edx, OFFSET prHiLo
+    call    WriteString
+    call    Crlf
+    jmp     getHigher
 
-
-	;start calcs
+    ; start calcs
 prepMath:
-	mov ecx, usLowr
-	mov ebx, usHigh				;exclusive
-	inc ebx						;inc b/c exclusive
-	jmp forLoop
+    mov     ecx, usLowr
+    dec     ecx                 ; decrement by 1 so that foorloop can include an inc  
+    mov     ebx, usHigh         ; exclusive
+    inc     ebx                 ; inc b/c exclusive
+    mov     edx, 0              ; prep for division
+    jmp     forLoop
+
+    ; do while ecx != ebx
+forLoop:
+    call    crlf
+
+    inc     ecx
+    cmp     ecx, ebx
+    je      continue
+
+    mov     eax, ecx
+    call    WriteDec
+    mov     edx, OFFSET colon
+    call    WriteString
+
+    mov     loopCt, 2
+    jmp     innerLoop
+
+  
 
 
+    ; run inner loop
+innerLoop:
+    cmp     loopCt, ecx           ; while j < i
+    jge      forLoop
 
-endProg:						; if the user doesn't want to do more calculations, say goodbye and quit
-	
-	
+    mov     eax, ecx
+    mov     edx, 0
+    div     loopCt
 
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;from template
-	exit ; exit to operating system
+    cmp     edx, 0                 ; check for remainder
+    je      printFactor
+
+    inc     loopCt
+    jmp     innerLoop             
+
+
+    ; if no remainder print factor
+printFactor:
+    mov     eax, loopCt
+    call    WriteDec
+    mov     edx, OFFSET space
+    call    WriteString
+
+    inc     loopCt
+    jmp     innerLoop
+
+    
+
+
+continue:
+
+endProg:
+
+    ; from template
+    exit ; exit to operating system
 main ENDP
+
 ; (insert additional procedures here)
 END main
